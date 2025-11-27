6a2eba8b34c361905437c85d475e788883c56b20
gowtham432 <64722931+gowtham432@users.noreply.github.com>
2025-11-27 11:59:44 -0600

    Update Spring Boot version and fix dependencies
    Updated Spring Boot version and dependencies to address vulnerabilities.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,38 +1,42 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "2.6.6"   // vulnerable: Spring4Shell
+    id("io.spring.dependency-management") version "1.1.4"
 }
 
 group = "com.myapp"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	implementation("com.fasterxml.jackson.core:jackson-databind:2.14.0")
-	runtimeOnly("mysql:mysql-connector-java:8.0.13")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.9.8") // critically vulnerable
+    runtimeOnly("mysql:mysql-connector-java:5.1.44") // multiple CVEs
+
     implementation("org.jetbrains:annotations:24.0.0")
     compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("com.mysql:mysql-connector-j")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
