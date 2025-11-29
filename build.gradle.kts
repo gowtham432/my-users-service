7b2d0971a35ea56925f324bc638f12c9a2e28ecc
gowtham432 <64722931+gowtham432@users.noreply.github.com>
2025-11-28 18:01:59 -0600

    Upgrade Spring Boot and dependencies in build.gradle.kts
    Updated Spring Boot and dependency management plugin versions. Updated annotations and springdoc-openapi dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,36 +1,43 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.2"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "3.3.5"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "com.myapp"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_21
+    sourceCompatibility = JavaVersion.VERSION_21
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-actuator")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-	implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
-	implementation("org.springframework.boot:spring-boot-starter-security")
-	implementation("org.springframework.boot:spring-boot-starter-validation")
-	implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.jetbrains:annotations:24.0.0")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // Latest annotations
+    implementation("org.jetbrains:annotations:24.1.0")
+
     compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
-	runtimeOnly("com.mysql:mysql-connector-j")
-	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("org.springframework.security:spring-security-test")
+    annotationProcessor("org.projectlombok:lombok")
+
+    runtimeOnly("com.mysql:mysql-connector-j:8.4.0")
+
+    // Fully compatible and secure version
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.6.0")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("org.springframework.security:spring-security-test")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
 }
